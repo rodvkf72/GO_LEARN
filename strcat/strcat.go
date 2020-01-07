f15f7e512fb693d3d5d703cd252c5727b9eaabd1
rodvkf72 <rhkdgh6@gmail.com>
2020-01-07 23:34:18 +0900

    strcat
    strcat

---
+++ strcat/strcat.go
@@ -0,0 +1,14 @@
+package strcat
+
+import "fmt"
+
+func Example_strCat() {
+	s := "abc"
+	ps := &s
+	s += "def"
+	fmt.Println(s)
+	fmt.Println(*ps)
+	//Output:
+	//abcdef
+	//abcdef
+}
---
+++ strcat/strcat_test.go
@@ -0,0 +1,9 @@
+package strcat
+
+import (
+	"testing"
+)
+
+func Test_Example_strCat(t *testing.T) {
+	Example_strCat()
+}
