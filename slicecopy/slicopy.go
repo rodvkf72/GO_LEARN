60ddd0702b23485cce39f11edb060acac6e6f8c7
rodvkf72 <rhkdgh6@gmail.com>
2020-01-09 20:31:21 +0900

    slice copy
    slicopy

---
+++ slicecopy/slicopy.go
@@ -0,0 +1,15 @@
+package slicecopy
+
+import "fmt"
+
+func Example_sliceCopy() {
+	src := []int{30, 20, 50, 10, 40}
+	dest := make([]int, len(src))
+	/*for i := range src {
+		dest[i] = src[i]
+	}*/
+	copy(dest, src)
+	fmt.Println(dest)
+	//Output:
+	//[30 20 50 10 40]
+}
---
+++ slicecopy/slicopy_test.go
@@ -0,0 +1,9 @@
+package slicecopy
+
+import (
+	"testing"
+)
+
+func Test_Example_sliceCopy(t *testing.T) {
+	Example_sliceCopy()
+}
