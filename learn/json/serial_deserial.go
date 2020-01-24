e903c4ebf1c3a6f23939319ba7173ae7b25be5f9
rodvkf72 <rhkdgh6@gmail.com>
2020-01-24 23:19:55 +0900

    json serial
    json serial

--- learn/json/serial_deserial.go
+++ learn/json/serial_deserial.go
@@ -11,23 +11,23 @@ type Deadline struct {
 	time.Time
 }
 
-type NewDeadline(t time.Time) *Deadline{
-  return &Deadline{t}
-}
-
 type status int
 
 type Task struct {
-	title    string
-	status   status
+	Title    string
+	Status   status
 	Deadline *Deadline
 }
 
-func Example_marshalJSON() {
+func NewDeadline(t time.Time) *Deadline {
+	return &Deadline{t}
+}
+
+func main() {
 	t := Task{
-		"Laundry",
-		2,
-		NewDeadline(time.Date(2020, time.January, 24, 22, 46, 0, 0, time.UTC)),
+		Title:    "Laundry",
+		Status:   2,
+		Deadline: NewDeadline(time.Date(2020, time.January, 24, 22, 46, 0, 0, time.UTC)),
 	}
 	b, err := json.Marshal(t)
 	if err != nil {
